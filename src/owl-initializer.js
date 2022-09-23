b2529c4720f85f8f0b772263d861e4650bd58522
Doğan Ay Şengül <20660110052@nevsehir.edu.tr>
2022-09-23 16:41:11 +0300

    mvp versiyon hazır

--- src/owl-initializer.js
+++ src/owl-initializer.js
@@ -4,6 +4,10 @@ $(document).ready(function(){
         loop:true,
         margin:0,
         dots:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+
         responsive:{
             0:{
                 items:1
@@ -22,6 +26,10 @@ $(document).ready(function(){
         loop:true,
         margin:0,
         nav:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+
         responsive:{
             0:{
                 items:6
@@ -35,12 +43,104 @@ $(document).ready(function(){
         },
         
     });
+
+    const owlThree = $('.owl-three').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+
+        responsive:{
+            0:{
+                items:6
+            },
+            600:{
+                items:6
+            },
+            1000:{
+                items:6
+            }
+        },
+        
+    });
+
+    const owlFour = $('.owl-four').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+
+        responsive:{
+            0:{
+                items:6
+            },
+            600:{
+                items:6
+            },
+            1000:{
+                items:6
+            }
+        },
+        
+    });
+
+    const owlFive = $('.owl-five').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+
+        responsive:{
+            0:{
+                items:6
+            },
+            600:{
+                items:6
+            },
+            1000:{
+                items:6
+            }
+        },
+        
+    });
+
     $('.prev').click(function() {
-        owl.trigger('prev.owl.carousel');
-    })
-    $('.next').click(function() {
         owl.trigger('next.owl.carousel');
     })
+    $('.next').click(function() {
+        owl.trigger('prev.owl.carousel');
+    })
+
+
+    $('.prev-three').click(function() {
+        owlThree.trigger('next.owl.carousel');
+    })
+    $('.next-three').click(function() {
+        owlThree.trigger('prev.owl.carousel');
+    })
+
+    
+    $('.prev-four').click(function() {
+        owlFour.trigger('next.owl.carousel');
+    })
+    $('.next-four').click(function() {
+        owlFour.trigger('prev.owl.carousel');
+    })
+
+    $('.prev-five').click(function() {
+        owlFive.trigger('next.owl.carousel');
+    })
+    $('.next-five').click(function() {
+        owlFive.trigger('prev.owl.carousel');
+    })
+
+
 
 });
 
