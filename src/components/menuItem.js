9868cdd68254184fc8c60f7fdf2997166814f520
Doğan Ay Şengül <20660110052@nevsehir.edu.tr>
2022-09-23 12:39:04 +0300

    slider ve carousel componentler ok

--- src/components/menuItem.js
+++ src/components/menuItem.js
@@ -64,3 +64,47 @@ window.customElements.define("inner-item", InnerItem);
 
 window.customElements.define("menu-item", MenuItem);
 
+const tab1 = document.querySelector("#tab-1");
+const tab2 = document.querySelector("#tab-2");
+const tab3 = document.querySelector("#tab-3");
+const slider2 = document.querySelector(".slider-2");
+const slider3 = document.querySelector(".slider-3");
+const slider4 = document.querySelector(".slider-4");
+
+tab1.addEventListener("click", function() {
+    slider2.style.display= "block";
+    slider3.style.display= "none";
+    slider4.style.display= "none";
+    
+
+    tab1.classList.add("checked-tab");
+    tab2.classList.remove("checked-tab");
+    tab3.classList.remove("checked-tab");
+
+});
+
+tab2.addEventListener("click", function() {
+    slider2.style.display= "none";
+    slider3.style.display= "block";
+    slider4.style.display= "none";
+    
+
+    
+    tab1.classList.remove("checked-tab");
+    tab2.classList.add("checked-tab");
+    tab3.classList.remove("checked-tab");
+
+});
+
+tab3.addEventListener("click", function() {
+    slider2.style.display= "none";
+    slider3.style.display= "none";
+    slider4.style.display= "block";
+    
+    
+    tab1.classList.remove("checked-tab");
+    tab2.classList.remove("checked-tab");
+    tab3.classList.add("checked-tab");
+});
+
+
--- src/owl-initializer.js
+++ src/owl-initializer.js
@@ -1,16 +1,50 @@
-$('.owl-carousel').owlCarousel({
-    margin:0,
-    center:true,
-    dots:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
+$(document).ready(function(){
+    
+    $('.owl-one').owlCarousel({
+        loop:true,
+        margin:0,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
         }
-    }
-})
+    });
+    
+
+    const owl = $('.owl-two').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        responsive:{
+            0:{
+                items:6
+            },
+            600:{
+                items:6
+            },
+            1000:{
+                items:6
+            }
+        },
+        
+    });
+    $('.prev').click(function() {
+        owl.trigger('prev.owl.carousel');
+    })
+    $('.next').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+
+});
+
+ 
+
+
+
